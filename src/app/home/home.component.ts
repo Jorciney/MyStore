4cfbd4a31eaeadd1b42d0674fc39ab298a2c12f4
Jorciney Dias Chaveiro <jorcineyadorador@hotmail.com>
2018-04-02 13:34:38 +0200

    changed product item and home component * using routerLink

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../shared/product';
+import {ProductService} from '../shared/product.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {
+  }
 
   ngOnInit() {
+    this.products = this.productService.getProducts();
   }
 
 }
