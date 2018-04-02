66ab01c500900904f8ef4cbfc930a1a78d74e29e
Jorciney Dias Chaveiro <jorcineyadorador@hotmail.com>
2018-04-02 20:26:20 +0200

    Finished product details

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Product} from '../shared/product';
+import {ActivatedRoute} from '@angular/router';
+import {ProductService} from '../shared/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,11 +9,14 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  product: Product;
 
-  constructor() {
+  constructor(private route: ActivatedRoute, private productService: ProductService) {
   }
 
   ngOnInit() {
+    const prodId: number = parseInt(this.route.snapshot.params['productId'], 0);
+    this.product = this.productService.getProductById(prodId);
   }
 
 }
